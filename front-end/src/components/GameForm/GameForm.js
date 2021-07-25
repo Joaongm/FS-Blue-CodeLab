862c1f8d9b1173667a76f1751fe5cdb645978112
David Sotto Mayor <ddsmmf@gmail.com>
2021-07-25 18:43:19 -0400

    Adicionando lógica de submissão em GameForm

--- front-end/src/components/GameForm/GameForm.js
+++ front-end/src/components/GameForm/GameForm.js
@@ -1,37 +1,95 @@
 import React, { Component, Fragment } from 'react';
 import classes from './GameForm.module.css';
-import Button from '../UI/Button/Button'
+import Button from '../UI/Button/Button';
+import { history } from 'react-router-dom'
 
 export class GameForm extends Component {
+
+    state = {
+        name: '',
+        url: '',
+        yearCreation: '',
+        description: '',
+    };
+
+    nameInputHandler = (event) => {
+        this.setState({ name: event.target.value });
+    };
+
+    urlInputHandler = (event) => {
+        this.setState({ url: event.target.value });
+    };
+
+    yearCreationInputHandler = (event) => {
+        this.setState({ yearCreation: event.target.value });
+    };
+
+    textareaHandler = (event) => {
+        this.setState({ description: event.target.value });
+    };
+
+    submitFormHandler = (event) => {
+        event.preventDefault();
+
+        this.setState({ name: '', url: '', yearCreation: '', description: '' });
+
+        console.log(this.state);
+        
+        
+
+    };
+
     render() {
         return (
             <Fragment>
-                <form className={classes.GameForm}>
+                <form
+                    onSubmit={this.submitFormHandler}
+                    className={classes.GameForm}
+                >
                     <div className={classes.formControl}>
                         <label>Nome</label>
-                        <input required type='text'/>
+                        <input
+                            value={this.state.name}
+                            onChange={this.nameInputHandler}
+                            required
+                            type="text"
+                        />
                     </div>
-                    
+
                     <div className={classes.formControl}>
                         <label>URL</label>
-                        <input required type='text'/>
+                        <input
+                            value={this.state.url}
+                            onChange={this.urlInputHandler}
+                            required
+                            type="text"
+                        />
                     </div>
-                    
+
                     <div className={classes.formControl}>
                         <label>Ano de criação</label>
-                        <input required type='text'/>
+                        <input
+                            value={this.state.yearCreation}
+                            onChange={this.yearCreationInputHandler}
+                            required
+                            type="text"
+                        />
                     </div>
-                    
+
                     <div className={classes.formControl}>
                         <label>Descrição</label>
-                        <textarea required></textarea>
+                        <textarea
+                            value={this.state.description}
+                            onChange={this.textareaHandler}
+                            required
+                        ></textarea>
                     </div>
-                    
+
                     <Button>Enviar</Button>
                 </form>
             </Fragment>
-        )
+        );
     }
 }
 
-export default GameForm
+export default GameForm;
